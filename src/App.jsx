9000407221bb3202dde704306cb06aa864bfa530
nishanthspk <spknishanth2004@gmail.com>
2024-02-22 22:22:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './Components/Nav'
+import Awareness from './Pages/Awareness'
+import Complaints from './Pages/Complaints'
+import Guidelines from './Pages/Guidelines'
+import Home from './Pages/Home'
+import News from './Pages/News'
+import Signin from './Pages/Signin'
+import MyFooter from './Components/Footer'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/news' element={<News />} />
+        <Route path='/awareness' element={<Awareness />} />
+        <Route path='/guidelines' element={<Guidelines />} />
+        <Route path='/complaints' element={<Complaints />} />
+        <Route path='/signin' element={<Signin />} />
+      </Routes>
+      <MyFooter />
+    </Router>
+  )
+}
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import logo from '../assets/Images/Logo.png';
+
+
+function myNavbar() {
+  return (
+    <Navbar expand="lg" className="bg-dark navbar-dark "  sticky="top" >
+      <Container>
+        <Navbar.Brand as={Link} to="/" className='fs-4 fw-medium ' >
+        <img src={logo} alt="Logo" height='50px' width='50px' className='me-2'/>
+         Green Guard</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav " />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto gap-4 ">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/news">News</Nav.Link>
+            <Nav.Link as={Link} to="/awareness">Awareness</Nav.Link>
+            <Nav.Link as={Link} to="/guidelines">Guidelines</Nav.Link>
+            <Nav.Link as={Link} to="/complaints">Complaints</Nav.Link>
+            <Nav.Link as={Link} to="/signin"><Button variant="success" className='ms-4'>Login</Button>{' '}</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default myNavbar;
